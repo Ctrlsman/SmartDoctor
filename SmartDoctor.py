d997db37dba492ba355982d43539d549058a6211
dongsheng <673711980@qq.com>
2018-05-04 18:17:36 +0800

    fix

--- SmartDoctor.py
+++ SmartDoctor.py
@@ -13,13 +13,14 @@ def check():
     timestamp = req_data.get("timestamp")
     nonce = req_data.get("nonce")
     echostr = req_data.get("echostr")
-    list = [token, timestamp, nonce]
-    list.sort()
-    sha1 = hashlib.sha1()
-    map(sha1.update, list)
-    hashcode = sha1.hexdigest()
+    l = [token, timestamp, nonce]
+    l.sort()
+    s = l[0] + l[1] + l[2]
+    hashcode = hashlib.sha1(s.encode()).hexdigest()
     if hashcode == signature:
         return echostr
+    if request.method == 'POST':
+        return 111
 
 
 if __name__ == '__main__':
