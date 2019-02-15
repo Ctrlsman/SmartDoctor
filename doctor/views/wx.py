71c5d66793d66eef9e0430dd54d8411dd4a10cdb
dongsheng <dongsheng.zhang@xinchan.com>
2019-02-15 10:46:43 +0800

    check kuaiqi

--- doctor/views/wx.py
+++ doctor/views/wx.py
@@ -6,6 +6,7 @@ import xml.etree.ElementTree as ET
 from doctor.utils.weixin import check_signature
 from doctor.response import wechat_response
 import time
+from doctor.logic.check_express import Express100
 
 bp = Blueprint('/wx', __name__)
 
@@ -21,6 +22,8 @@ def handle_wechat_request():
         ToUserName = xml_recv.find("ToUserName").text
         FromUserName = xml_recv.find("FromUserName").text
         Content = xml_recv.find("Content").text
+        if str(Content).startswith('快递'):
+            Content = Express100.get_express_info(str(Content).split(':')[1].strip())
         reply = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
         response = make_response(reply % (FromUserName, ToUserName, str(int(time.time())), Content))
         response.content_type = 'application/xml'
